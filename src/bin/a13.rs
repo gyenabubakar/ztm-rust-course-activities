0ce83e92abb1e11f159316202cf9f50b8c14d9ac
Gyen Abubakar <indehyde@gmail.com>
2023-02-11 09:31:24 +0000

    reset activity 13

--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -10,16 +10,4 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {
-    let numbers = vec![10, 20, 30, 40];
-
-    for num in &numbers {
-        if *num == 30 {
-            println!("thirty");
-            continue;
-        }
-        println!("{:?}", &num);
-    }
-
-    println!("`numbers` size: {:?}", numbers.len())
-}
+fn main() {}
