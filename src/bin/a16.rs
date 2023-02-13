60b51677ae85b6e0b1a2fb80c899257616a37bc5
Gyen Abubakar <indehyde@gmail.com>
2023-02-13 04:16:28 +0000

    solved activity 16: Option type

--- src/bin/a16.rs
+++ src/bin/a16.rs
@@ -8,7 +8,29 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
+struct Student {
+    name: String,
+    locker_assignment: Option<i32>,
+}
 
-fn main() {}
+fn main() {
+    let students = vec![
+        Student {
+            name: "Gyen".to_owned(),
+            locker_assignment: Some(30),
+        },
+        Student {
+            name: "Kwame".to_owned(),
+            locker_assignment: None,
+        },
+    ];
+
+    for stud in students {
+        println!("Student name: {}", stud.name);
+        print!("Locker assignment: ");
+        match stud.locker_assignment {
+            Some(value) => println!("{}\n", value),
+            None => println!("Has none\n")
+        }
+    }
+}
