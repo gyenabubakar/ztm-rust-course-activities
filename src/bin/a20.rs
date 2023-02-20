dbe242ff37adb50119aae914ad9a5c17c001cc2d
Gyen Abubakar <indehyde@gmail.com>
2023-02-20 21:13:45 +0000

    solved activity 20: user input

--- src/bin/a20.rs
+++ src/bin/a20.rs
@@ -23,4 +23,70 @@
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
 
-fn main() {}
+#![allow(dead_code)]
+
+use std::io;
+
+#[derive(Copy, Clone)]
+enum PowerOption {
+    Off,
+    Sleep,
+    Reboot,
+    Shutdown,
+    Hibernate,
+}
+
+impl PowerOption {
+    fn new(keyword: &str) -> Option<Self> {
+        let owned = keyword.to_owned().to_lowercase();
+        let trimmed = owned.trim();
+        match trimmed {
+            "off" => Some(Self::Off),
+            "sleep" => Some(Self::Sleep),
+            "reboot" => Some(Self::Reboot),
+            "shutdown" => Some(Self::Shutdown),
+            "hibernate" => Some(Self::Hibernate),
+            _ => None
+        }
+    }
+}
+
+fn get_input() -> io::Result<Option<PowerOption>> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+
+    let keyword = buffer.trim();
+    let option = PowerOption::new(keyword);
+    Ok(option)
+}
+
+fn print_power_message(opt: Option<PowerOption>) {
+    use PowerOption::*;
+
+    match opt {
+        None => println!("You entered an invalid keyword."),
+        Some(option) => {
+            let message: &str;
+            match option {
+                Off => message = "going off...",
+                Sleep => message = "sleeping...",
+                Reboot => message = "rebooting",
+                Shutdown => message = "shutting down",
+                Hibernate => message = "hibernating"
+            }
+            println!("{message}")
+        }
+    }
+}
+
+fn main() {
+    println!("Enter keyword: [off, sleep, reboot, shutdown, hibernate] 👇");
+
+    let option = get_input();
+    if option.is_err() {
+        println!("ERR: {:?}", option.err());
+        return;
+    }
+
+    print_power_message(option.unwrap());
+}
