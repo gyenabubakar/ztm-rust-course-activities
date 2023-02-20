e1cc8bb0c75e9398406e9488b479f1e01dfab9c3
Gyen Abubakar <indehyde@gmail.com>
2023-02-20 12:11:28 +0000

    solved activity 26: external crates

--- src/bin/a26.rs
+++ src/bin/a26.rs
@@ -4,8 +4,14 @@
 // * Display the current date and time
 //
 // Notes:
-// * Use the `chrono` crate to work with time
+// * Use the `chrono` crate to work with ti me
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
+use chrono::{Datelike, Local};
 
-fn main() {}
+fn main() {
+    let date = Local::now();
+    let readable = date.format("%A, %d %B, %Y");
+    println!("{}", readable.to_string());
+    println!("date–{} month–{} year–{}", date.day(), date.month(), date.year())
+}
